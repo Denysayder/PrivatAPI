29735b1544c5ff864a302428400824639070fc97
Denys <borovikdenis45@gmail.com>
2022-06-27 20:38:41 +0300

    delBot

--- Course_2/TG_bot/TG_bot/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TG_bot;
-
-EasyPrivatBank_bot easyPrivatBank_Bot = new EasyPrivatBank_bot();
-easyPrivatBank_Bot.Start();
-Console.ReadKey();
-
